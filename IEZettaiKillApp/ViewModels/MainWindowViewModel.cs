f4f8cf6a3d12e5a86578b47dc13c81ff991f24a5
shuntaro4 <shuntaro4@gmail.com>
2019-09-28 00:13:27 +0900

    add IEZettaiKill command.

--- IEZettaiKillApp/ViewModels/MainWindowViewModel.cs
+++ IEZettaiKillApp/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,39 @@
 ﻿using Prism.Mvvm;
 using Reactive.Bindings;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace IEZettaiKillApp.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
         public ReactiveProperty<string> Title { get; } = new ReactiveProperty<string>("IEZettaiKillApp");
+
+        public ReactiveProperty<string> Message { get; set; } = new ReactiveProperty<string>("IE Kill Counter: 0");
+
+        public ReactiveCommand IEZettaiKillCommand { get; }
+
+        private int ieKillCount;
+
+        public MainWindowViewModel()
+        {
+            IEZettaiKillCommand = new ReactiveCommand();
+            IEZettaiKillCommand.Subscribe(IEZettaiKillAction);
+        }
+        private async void IEZettaiKillAction()
+        {
+            while (true)
+            {
+                var processes = Process.GetProcesses();
+                var ieProcesses = processes.Where(x => x.ProcessName.Contains("iexplore"));
+                foreach (var ie in ieProcesses)
+                {
+                    ie.Kill(true);
+                    Message.Value = $"IE Kill Counter:{++ieKillCount}";
+                }
+                await Task.Delay(1000);
+            }
+        }
     }
 }
