66e7bea981c172bd97f5240a400cefc8fea15053
shuntaro4 <shuntaro4@gmail.com>
2019-09-28 01:09:18 +0900

    add ctrl+W shutdown action.

--- IEZettaiKillApp/View/MainWindow.xaml.cs
+++ IEZettaiKillApp/View/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ namespace IEZettaiKillApp.Views
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
-            Close();
+            Shutdown();
         }
 
         private void DragMove(object sender, MouseButtonEventArgs e)
@@ -33,6 +33,22 @@ namespace IEZettaiKillApp.Views
         {
             // after clicking the button, clear the focus.
             Keyboard.ClearFocus();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            var key = e.Key;
+            var modifiersKey = Keyboard.Modifiers;
+
+            if (modifiersKey == ModifierKeys.Control && key == Key.W)
+            {
+                Shutdown();
+            }
+        }
+
+        private void Shutdown()
+        {
+            Application.Current.Shutdown(1995816);
         }
     }
 }
