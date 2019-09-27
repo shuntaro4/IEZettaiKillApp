be2ab075db9b2d04b565e236cebee49ac835bd12
shuntaro4 <shuntaro4@gmail.com>
2019-09-28 00:33:30 +0900

    add IEZettaiKillStartStop command.

--- IEZettaiKillApp/ViewModels/MainWindowViewModel.cs
+++ IEZettaiKillApp/ViewModels/MainWindowViewModel.cs
@@ -14,19 +14,33 @@ namespace IEZettaiKillApp.ViewModels
 
         public ReactiveProperty<string> Message { get; } = new ReactiveProperty<string>("Kill Counter: 0");
 
+        public ReactiveProperty<string> StartStopIcon { get; } = new ReactiveProperty<string>("■");
+
         public ReactiveCommand IEZettaiKillCommand { get; }
 
+        public ReactiveCommand IEZettaiKillStartStopCommand { get; }
+
         private int ieKillCount;
+
+        private bool killIE = true;
 
         public MainWindowViewModel()
         {
             IEZettaiKillCommand = new ReactiveCommand();
             IEZettaiKillCommand.Subscribe(IEZettaiKillAction);
+
+            IEZettaiKillStartStopCommand = new ReactiveCommand();
+            IEZettaiKillStartStopCommand.Subscribe(IEZettaiKillStartStopAction);
         }
         private async void IEZettaiKillAction()
         {
             while (true)
             {
+                await Task.Delay(1000);
+                if (!killIE)
+                {
+                    continue;
+                }
                 var processes = Process.GetProcesses();
                 var ieProcesses = processes.Where(x => x.ProcessName.Contains("iexplore"));
                 foreach (var ie in ieProcesses)
@@ -34,7 +48,21 @@ namespace IEZettaiKillApp.ViewModels
                     ie.Kill(true);
                     Message.Value = $"Kill Counter:{++ieKillCount}";
                 }
-                await Task.Delay(1000);
+            }
+        }
+
+        private void IEZettaiKillStartStopAction()
+        {
+            killIE = !killIE;
+            if (killIE)
+            {
+                StartStopIcon.Value = "■";
+                StatusMessage.Value = "IE Monitoring...";
+            }
+            else
+            {
+                StartStopIcon.Value = "▶";
+                StatusMessage.Value = "Stop";
             }
         }
     }
