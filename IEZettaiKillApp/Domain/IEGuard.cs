174943d2c8d53a14ff51c5f291e0b4e6ad8bb2c2
shuntaro4 <shuntaro4@gmail.com>
2019-10-06 23:59:05 +0900

    add DefaultBrowserIsIE method.

--- IEZettaiKillApp/Domain/IEGuard.cs
+++ IEZettaiKillApp/Domain/IEGuard.cs
@@ -4,6 +4,16 @@ namespace IEZettaiKillApp.Domain
 {
     public class IEGuard
     {
+        public bool DefaultBrowserIsIE()
+        {
+            var name = GetDefaultBrowser();
+            if (name.Contains("IE"))
+            {
+                return true;
+            }
+            return false;
+        }
+
         public string GetDefaultBrowser()
         {
             var subkey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice");
