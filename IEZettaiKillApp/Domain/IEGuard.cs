3c8835e2c7287acdaae5c68ed3e6e2724606ccb3
shuntaro4 <shuntaro4@gmail.com>
2019-10-06 23:51:31 +0900

    add IEGuard class.

---
+++ IEZettaiKillApp/Domain/IEGuard.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Win32;
+
+namespace IEZettaiKillApp.Domain
+{
+    public class IEGuard
+    {
+        public string GetDefaultBrowser()
+        {
+            var subkey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice");
+            var name = subkey?.GetValue("ProgId") as string;
+            return name;
+        }
+    }
+}
