f02212a6a622279a54b80d7e1c1b5433fb3d41da
shuntaro4 <shuntaro4@gmail.com>
2019-12-31 14:27:52 +0900

    add DefaultBrowserService class.

---
+++ IEZettaiKillApp.Core/ApplicationService/DefaultBrowserService.cs
@@ -0,0 +1,14 @@
+﻿using IEZettaiKillApp.Core.ApplicationService.Interfaces;
+using IEZettaiKillApp.Core.Domain;
+
+namespace IEZettaiKillApp.Core.ApplicationService
+{
+    public class DefaultBrowserService : IDefaultBrowserService
+    {
+        public bool IsIE()
+        {
+            var defaultBrowser = new DefaultBrowser();
+            return defaultBrowser?.IsIE ?? false;
+        }
+    }
+}
