ccc0bdde80bd026d9eac0b66c6616affc25dc7dd
shuntaro4 <shuntaro4@gmail.com>
2019-12-31 13:43:38 +0900

    add DefaultBrowser class.

---
+++ IEZettaiKillApp.Core/Domain/DefaultBrowser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Win32;
+
+namespace IEZettaiKillApp.Core.Domain
+{
+    public class DefaultBrowser
+    {
+        private WebBrowserType type;
+
+        public bool IsIE
+        {
+            get
+            {
+                if (type == WebBrowserType.IE)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public DefaultBrowser()
+        {
+            var subkey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice");
+            var name = subkey?.GetValue("ProgId") as string ?? "";
+            type = name.Contains("IE") ? WebBrowserType.IE : WebBrowserType.OTHER;
+        }
+    }
+}
