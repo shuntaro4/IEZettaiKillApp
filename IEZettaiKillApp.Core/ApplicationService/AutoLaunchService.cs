c22ef0320f89b63b05743bdfcafe5875bcdcf0dc
shuntaro4 <shuntaro4@gmail.com>
2019-12-31 15:18:41 +0900

    add AutoLaunchService class.

---
+++ IEZettaiKillApp.Core/ApplicationService/AutoLaunchService.cs
@@ -0,0 +1,25 @@
+﻿using IEZettaiKillApp.Core.ApplicationService.Interfaces;
+using IEZettaiKillApp.Core.Infrastructure;
+
+namespace IEZettaiKillApp.Core.ApplicationService
+{
+    public class AutoLaunchService : IAutoLaunchService
+    {
+        private RunRegister runRegister;
+
+        public AutoLaunchService()
+        {
+            runRegister = new RunRegister();
+        }
+
+        public void SetAutoLaunchSetting()
+        {
+            runRegister.RegistKey(true);
+        }
+
+        public void CancelAutoLaunchSetting()
+        {
+            runRegister.RegistKey(false);
+        }
+    }
+}
