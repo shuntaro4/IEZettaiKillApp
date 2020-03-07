cdd9a288ae6bbde626fac7b972c9b1af64f8ab9b
shuntaro4 <shuntaro4@gmail.com>
2020-03-07 14:51:37 +0900

    add ScreenInfoService class.

---
+++ IEZettaiKillApp.Core/ApplicationService/Interfaces/IScreenInfoService.cs
@@ -0,0 +1,9 @@
+﻿using IEZettaiKillApp.Core.Domain;
+
+namespace IEZettaiKillApp.Core.ApplicationService.Interfaces
+{
+    public interface IScreenInfoService
+    {
+        ScreenInfo GetPrimaryScreenInfo();
+    }
+}
